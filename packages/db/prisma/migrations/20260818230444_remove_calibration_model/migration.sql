5bb8c66b68d0100035e12983d99ce45b05f26c9e
Fannan Luc Coulibaly <46203387+coulbyl@users.noreply.github.com>
2026-08-18 23:09:48 +0000

    chore(migration): add SQL migration to drop calibration_report and channel_tuning_result tables

---
+++ packages/db/prisma/migrations/20260818230444_remove_calibration_model/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `calibration_report` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `channel_tuning_result` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "calibration"."calibration_report";
+
+-- DropTable
+DROP TABLE "calibration"."channel_tuning_result";
